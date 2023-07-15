6d16ddb7638b46ed8b8350401c6b8a2983cf7dbb
edel-blk <stanminesilver@gmail.com>
2023-07-14 20:01:41 -0700

    tic tac toe

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 const TURNS = {
@@ -5,9 +6,78 @@ const TURNS = {
   O: 'o'
 }
 
-const board = Array(9).fill(null);
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+const Square = ({ children, isSelected, updatedBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+  const handleClick = () => {
+    updatedBoard(index);
+  }
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const checkEndGame = (newBoard) => {
+  // revisamos si hay un empate
+  // si no hay más espacios vacíos
+  // en el tablero
+  return newBoard.every((square) => square !== null)
+}
 
 function App() {
+
+  const [ board, setBoard] = useState(Array(9).fill(null));
+
+  const [ turn, setTurn] = useState(TURNS.X);
+
+  const [ winner, setWinner] = useState(null);
+
+  const checkWinnerFrom = (boardToCheck) => {
+    // revisamos todas las combinaciones ganadoras
+    // para ver si X u O ganó
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+  }
+
+  const updatedBoard = (index) => {
+    if (board[index] || winner) return;
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    const newWinner = checkWinnerFrom(newBoard);
+    if (newWinner) {
+      setWinner(true)
+    }
+  }
+
   return (
     <main className='board'>
       <h1>Tic tac toe</h1>
@@ -15,14 +85,26 @@ function App() {
         {
           board.map((_, index) => {
             return (
-              <div className='cell' key={index}>
-                <span className='cell_content'>
-                  {index}
-                </span>
-              </div>
+              <Square
+                key={index}
+                index={index}
+                updatedBoard={updatedBoard}
+              >
+                {board[index]}
+              </Square>
             )
           })
         }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   )
